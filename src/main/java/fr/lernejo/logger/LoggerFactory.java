515847ffcd5b94f18e105c0a3ee525fbd8433ed8
Mathis Cadio <cadio@et.esiea.fr>
2021-12-01 11:50:07 +0100

    Partie 2 - Création d’un Logger

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,7 @@
+package fr.lernejo.logger;
+
+public abstract class LoggerFactory implements Logger {
+    public static Logger getLogger(String name) {
+        return new ConsoleLogger();
+    }
+}
