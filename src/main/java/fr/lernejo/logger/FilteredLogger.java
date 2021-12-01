6338494563f3ef43a89d3c842f57540a551a4044
Mathis Cadio <cadio@et.esiea.fr>
2021-12-01 15:38:01 +0100

    Partie 7 - Encore plus de composition

---
+++ src/main/java/fr/lernejo/logger/FilteredLogger.java
@@ -0,0 +1,19 @@
+package fr.lernejo.logger;
+
+import java.util.function.Predicate;
+
+public class FilteredLogger implements Logger {
+    private final Logger delegate;
+    private final Predicate<String> condition;
+
+    public FilteredLogger(Logger delegate, Predicate<String> condition) {
+        this.delegate = delegate;
+        this.condition = condition;
+    }
+
+    public void log(String message) {
+        if (condition.test(message)) {
+            this.delegate.log(message);
+        }
+    }
+}
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,6 +2,6 @@ package fr.lernejo.logger;
 
 public abstract class LoggerFactory implements Logger {
     public static Logger getLogger(String name) {
-        return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FileLogger("src/main/java/fr/lernejo/logger/log.txt")));
+        return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FilteredLogger(new FileLogger("src/main/java/fr/lernejo/logger/log.txt"), condition -> name.equals("simulation"))));
     }
 }
